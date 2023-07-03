631cd4b0328a6b071681b8652509f78ea393a8c7
Alexander <alexander.cave@nationwide.co.uk>
2023-07-03 11:59:23 +0100

    lists

--- Jarvascript/script.js
+++ Jarvascript/script.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+// console.log("hello world")
+
+let mordor=["one", "does", "not","simpley", "walk", "into", "mordor"];
+// console.log(mordor);
+
+mordor.pop();
+// console.log(mordor);
+
+mordor.push("Rivendale");
+// console.log(mordor);
+
+mordor.splice(7,"master", "bagins", "said", "boramir");
+// console.log(mordor);
+
+mordor.reverse();
+// console.log(mordor);
+
+mordor.unshift("today");
+// console.log(mordor);
+
+mordor.shift();
+// console.log(mordor);
+
+
+console.log(mordor.join("gandulf"));
