d1696361e8a64cfb6059845fa29c5ea06618d177
Alexander <alexander.cave@nationwide.co.uk>
2023-07-03 17:19:06 +0100

    endofday

--- Jarvascript/script.js
+++ Jarvascript/script.js
@@ -1,25 +1,73 @@
 // console.log("hello world")
 
-let mordor=["one", "does", "not","simpley", "walk", "into", "mordor"];
+// let mordor=["one", "does", "not","simpley", "walk", "into", "mordor"];
 // console.log(mordor);
 
-mordor.pop();
+// mordor.pop();
 // console.log(mordor);
 
-mordor.push("Rivendale");
+// mordor.push("Rivendale");
 // console.log(mordor);
 
-mordor.splice(7,"master", "bagins", "said", "boramir");
+// mordor.splice(7,"master", "bagins", "said", "boramir");
 // console.log(mordor);
 
-mordor.reverse();
+// mordor.reverse();
 // console.log(mordor);
 
-mordor.unshift("today");
+// mordor.unshift("today");
 // console.log(mordor);
 
-mordor.shift();
+// mordor.shift();
 // console.log(mordor);
 
 
-console.log(mordor.join("gandulf"));
+// console.log(mordor.join("gandulf"));
+
+
+// objects
+
+const car={
+    Name:  "Jill",
+    Model: "Fiesta",
+    Brand: "Ford",
+    Reg: "kj45 9vl",
+};
+
+const car1={
+    Name:  "Andy",
+    Model: "Boxter",
+    Brand: "Porche",
+    Reg: "rf82 1nj",
+};
+
+const car2={
+    Name:  "Sally",
+    Model: "Polo",
+    Brand: "VW",
+    Reg: "gh49 0mg",
+};
+
+const car3={
+    Name:  "Bill",
+    Model: "Micra",
+    Brand: "Nissan",
+    Reg: "nc20 8il",
+};
+
+console.log(car);
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
+car3.Reg="mc20 9il"; 
+// amended the property
+console.log(car3);
+
+car3.doors=3;
+console.log(car3);
+
+console.log(car3["Reg"]);
+
+
+
