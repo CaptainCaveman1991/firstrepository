2aa72e51ada97b645d63225bd45e0e74d03e3d1e
alexander <alexander.cave@nationwide.co.uk>
2023-07-24 10:46:43 +0100

    eod

--- express/project5/server.js
+++ express/project5/server.js
@@ -7,28 +7,47 @@ let db=MySql.createConnection({
     user:"root",
     password:"root",
     database:"nbs"
+let Express=require("express")
+
+let server=Express()
+
+let MySql=require("mysql")
+
+let db=MySql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"nbs"
 })
+
 db.connect()
 
-server.get("/",function(req,res){
-    res.render("homepage.ejs")
+
+server.get("/addEmployee",funcion(re,res){
+        res.render("Forms.ejs")
+
+})
+
+server.get("/employeesOfDepartment/:dept",function(req,res){
+    let sql1="select * from employees where department='"+req.params.dept+"'"
+    db.query(sql1,function(err,records){
+    res.render("employeeslist.ejs",{employees:records})
     res.end()
 })
-
-server.get("/allemploymeeslist",function(req,res){
-    db.query("select * from emplyees",function(eer,records){
-        res.render("employeeslist.ejs",{emplyee:records})
-        res.end
-    })
 })
 
-server.get("/departments",function(req,res){
-        db.query("Select distinct department from employees",function(err,records){
-            res.render("departments.ejs",{ deptName:records})  
-            res.end()
+server.get("/deleteEmployee/:empno",function(req,res){
+    let deleteSQL="delete from employees where empno="+ req.params.empno
+    db.query(deleteSQL,function(err,data){
 
-     })
- })
+db.query("select * from employees",function(err,records){
+res.render("employeeslist.ejs",{employees:records}
+res.end()
 
-server.listen(3000)
-    
+})
+})
+})
+
+server.get("/",function(req,res){
+})
+})
